27e64bc4a97254b6fc786e9f1e47ab8aad83dbe7
Hristoz Ognyanov <hristozognyanov1984@gmail.com>
2024-01-20 13:44:45 +0100

    Show Dynamic Movies on Home page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -7,13 +7,18 @@ const movies = [
         year: '2023',
         imageUrl:'https://resizing.flixster.com/G2ypcmeD3YeumP2nvKiEmO5XQok=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2Y5MmJmNTk0LTFjNGEtNDVhNC1iNjI3LTFmNTVhYjg1ODc5Yy5qcGc=',
         rating: '8.5',
-        discription: "One man's brutal campaign for vengeance takes on national stakes after he is revealed to be a former operative of a powerful and clandestine organization known as Beekeepers."
+        description: "One man's brutal campaign for vengeance takes on national stakes after he is revealed to be a former operative of a powerful and clandestine organization known as Beekeepers."
 
     }
 
 ];
 
 
+exports.getAll = () => {
+    return movies.slice();
+}
+
 exports.create = (movieData) => {
      movies.push(movieData);
 }
+
