f57475f6d31eee2a05155966229cac8f084d56bc
Hristoz Ognyanov <hristozognyanov1984@gmail.com>
2024-01-20 10:32:57 +0100

    Add express handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 const PORT = 5050;
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+app.engine('hbs', handlebars.engine({ 
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.send('Home Page')
+    res.render('home', {layout: false})
 });
 
 app.listen(PORT, () => console.log(`Server is runing on port ${PORT}...`));
