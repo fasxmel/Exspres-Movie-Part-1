30bc02ece928181fb73120654a094c4decd18a16
Hristoz Ognyanov <hristozognyanov1984@gmail.com>
2024-01-21 14:31:42 +0100

    Show Movies on home page

--- src/models/movie.js
+++ src/models/movie.js
@@ -2,36 +2,14 @@ const { Schema, model } = require('mongoose');
 
 
 const movieSchema = new Schema({
-    title: {
-        type: 'String',
-       
-    },
-    genre: {
-        type: 'String',
-       
-    },
-    director: {
-        type: 'String',
-     
-    },
-    year: {
-        type: 'Number',
-       
-    },
-    imageUrl: {
-        type: 'String',
-      
-    },
-    rating: {
-        type: 'Number',
-       
-      
-       
-    },
-    description: {
-        type: 'String',
-    },
-
+    title: 'String',
+    genre: 'String',
+    director: 'String',
+    year: 'Number',
+    imageUrl: 'String',
+    rating: 'Number',
+    description: 'String',
+    
 });
 
 const Movie = model('Movie', movieSchema);
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,55 +1,61 @@
-const movies = [
-
-    {
-        _id: 1,
-        title: 'The Beekeeper',
-        genre: 'Action',
-        director: 'David Ayer',
-        year: 2023,
-        imageUrl:'https://resizing.flixster.com/G2ypcmeD3YeumP2nvKiEmO5XQok=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2Y5MmJmNTk0LTFjNGEtNDVhNC1iNjI3LTFmNTVhYjg1ODc5Yy5qcGc=',
-        rating: '8.5',
-        description: "One man's brutal campaign for vengeance takes on national stakes after he is revealed to be a former operative of a powerful and clandestine organization known as Beekeepers."
-
-    }
-
-];
+const Movie = require('../models/movie');
 
 
-exports.getAll = () => {
-    return movies.slice();
+
+// const movies = [
+
+//     {
+//         _id: 1,
+//         title: 'The Beekeeper',
+//         genre: 'Action',
+//         director: 'David Ayer',
+//         year: 2023,
+//         imageUrl:'https://resizing.flixster.com/G2ypcmeD3YeumP2nvKiEmO5XQok=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2Y5MmJmNTk0LTFjNGEtNDVhNC1iNjI3LTFmNTVhYjg1ODc5Yy5qcGc=',
+//         rating: '8.5',
+//         description: "One man's brutal campaign for vengeance takes on national stakes after he is revealed to be a former operative of a powerful and clandestine organization known as Beekeepers."
+
+//     }
+
+// ];
+
+
+exports.getAll = async () => {
+   let movies = await Movie.find().lean();
+
+   return movies;
 }
 
-exports.getOne = (movieId) => {
-    const movie =  result.find(movie => movie._id == movieId);
+// exports.getOne = (movieId) => {
+//     const movie =  result.find(movie => movie._id == movieId);
    
-   return movie;
-}
+//    return movie;
+// }
 
-exports.search = (title, genre, year) => {
-    let result =  movies.slice();
+// exports.search = (title, genre, year) => {
+//     let result =  movies.slice();
    
-   if (title) {
-    result = result.filter(movie => movie.title.toLocaleLowerCase().includes(title.toLocaleLowerCase())); 
-   }
+//    if (title) {
+//     result = result.filter(movie => movie.title.toLocaleLowerCase().includes(title.toLocaleLowerCase())); 
+//    }
 
-   if (genre) {
-    result = result.filter(movie => movie.genre.toLocaleLowerCase() === genre.toLocaleLowerCase()); 
-   }
+//    if (genre) {
+//     result = result.filter(movie => movie.genre.toLocaleLowerCase() === genre.toLocaleLowerCase()); 
+//    }
 
-   if (year) {
-    result = result.filter(movie => movie.year === year); 
-   }
+//    if (year) {
+//     result = result.filter(movie => movie.year === year); 
+//    }
 
-   return result;
-}
+//    return result;
+// }
 
 
 
-exports.create = (movieData) => {
-    movieData._id = movies[movies.length - 1]._id + 1;
+// exports.create = (movieData) => {
+//     movieData._id = movies[movies.length - 1]._id + 1;
 
-     movies.push(movieData);
-}
+//      movies.push(movieData);
+// }
 
 
 
