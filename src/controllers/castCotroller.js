8b5c2a54fc2ddbc4822b24412937d269ae27c6ea
Hristoz Ognyanov <hristozognyanov1984@gmail.com>
2024-01-27 17:30:07 +0100

    add cast page

--- src/controllers/castCotroller.js
+++ src/controllers/castCotroller.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
 
-router.get('cast/create', (req, res) => {
-      res.render()
+router.get('/create', (req, res) => {
+      res.render('cast/create');
 });
 
 
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const movieController = require('./controllers/movieController');
+const castCotroller = require('./controllers/castCotroller');
 
 router.use(homeController);
 router.use(movieController);
+router.use('/cast',castCotroller);
 
 
 router.get('*', (req, res) => {
