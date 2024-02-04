3730f2cd36e66b69a9b1ce8369816e77a8a2c6d2
Hristoz Ognyanov <hristozognyanov1984@gmail.com>
2024-02-04 12:14:43 +0100

    show register page

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const movieController = require('./controllers/movieController');
 const castCotroller = require('./controllers/castCotroller');
+const userController = require('./controllers/userController');
 
 router.use(homeController);
 router.use(movieController);
 router.use('/cast',castCotroller);
+router.use('/user',userController);
 
 
 router.get('*', (req, res) => {
